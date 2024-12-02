d18d9f6752c952ab4b2a4baf8842fbac2d2ddb8d
Bren <bpgit503@gmail.com>
2024-12-02 20:33:59 +0100

    Spring context basic dependency injection

--- src/main/java/guru/springframework/spring6di/controller/MyController.java
+++ src/main/java/guru/springframework/spring6di/controller/MyController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 public class MyController {
 
     public String sayHello() {
+
         System.out.println("Hello I am MyController");
+
         return "Salam Malaykum";
     }
 }
